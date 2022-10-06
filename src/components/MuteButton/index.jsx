d35ab1f7dcacd62143a265c8f635f28357be3fec
Wilson Mesquita <wilsonbrasilplauforever@gmail.com>
2022-10-05 23:50:40 -0300

    fix: bug audio duplicado

--- src/components/MuteButton/index.jsx
+++ src/components/MuteButton/index.jsx
@@ -5,7 +5,7 @@ import React from "react"
 const MuteButton = (props) => {
 
     return (
-        <button onClick={props.onClick} style={{background: "none", border: "none"}}>{props.muted ? <GiSoundOff size={50}/> : <GiSoundOn size={50} />}</button>
+        <button onClick={props.onClick} style={{background: "none", border: "none"}}>{props.muted ? <GiSoundOn size={50} />  : <GiSoundOff size={50}/>}</button>
     )
 }
 
