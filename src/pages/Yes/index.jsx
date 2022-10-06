43dbd92695a716955f34ca2e64c99973d1a4093c
Wilson Mesquita <wilsonbrasilplauforever@gmail.com>
2022-10-05 23:39:00 -0300

    feat: audio button dinamico

--- src/pages/Yes/index.jsx
+++ src/pages/Yes/index.jsx
@@ -1,12 +1,45 @@
 import Confetti from "../../components/Confetti"
 import { YesContainer } from "./styles"
+import music from "../../audio/music.mp3"
+import { TypeAnimation } from "react-type-animation"
+import { useState } from "react"
+import MuteButton from "../../components/MuteButton"
+
 
 const Yes = () => {
+    const [mute, setMute] = useState(false)
+
+    const functionMute = (e) => {
+        e.preventDefault()
+
+        setMute((oldMute) => !oldMute)
+
+        const audio = document.getElementById('player')
+        console.log(audio)
+
+        audio[mute ? "pause" : "play"]()
+
+    }   
+
     return (
         <YesContainer>
+            <MuteButton muted={mute} onClick={(e) => functionMute(e)} />
             <Confetti>
-                Chama no zap bb
+                <TypeAnimation
+                    sequence={[
+                        `Chama no zap bb`,
+                        3000
+                    ]}
+                wrapper="div"
+                cursor={true}
+                repeat={false}
+                style={{ fontSize: '1.5em' }}
+                />
             </Confetti>
+            <iframe title="audio" src={music} allow="autoplay" id="audio" style={{display: "none"}}></iframe>
+            <audio id="player" autoPlay loop>
+                <source src={music} type="audio/mp3"/>
+            </audio>
         </YesContainer>
     )
 }
